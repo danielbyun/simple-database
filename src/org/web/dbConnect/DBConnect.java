a2df1e7c2a1d386f1cbec3920dc685f24590b3a1
Daniel Byun <dbyun92@gmail.com>
2018-12-20 10:53:15 +0900

    update DBConnect.java

--- src/org/web/dbConnect/DBConnect.java
+++ src/org/web/dbConnect/DBConnect.java
@@ -10,14 +10,7 @@ import java.sql.SQLException;
 public class DBConnect {
     public static Connection getConnection() {
         Connection conn = null;
-//        String url = "jdbc:mysql://localhost:3306/webprojects?allowPublicKeyRetrieval=true&useSSL=false&amp&useUnicode=true&amp&serverTimezone=UTC";
-//        String hostId = "root";
-//        String hostPw = "12345678";
-//        String driver = "com.mysql.cj.jdbc.Driver";
-
         try {
-//            Class.forName(driver);
-//            conn = DriverManager.getConnection(url, hostId, hostPw);
             Context initContext = new InitialContext();
             Context envContext = (Context) initContext.lookup("java:/comp/env");
             DataSource ds = (DataSource) envContext.lookup("jdbc/db");
